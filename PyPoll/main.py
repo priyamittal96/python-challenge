404d2b8a98eef5715f4373503d92d5f3262a2496
shantipriya30 <75239249+shantipriya30@users.noreply.github.com>
2021-01-05 16:59:32 -0800

    Update scrupting file with candidate counts

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -5,6 +5,7 @@ people =[]
 candidates = []
 percentage = []
 winner = []
+results = [0,0,0,0]
 
 count = 0
 
@@ -16,15 +17,26 @@ with open(poll_csv) as poll_file:
         candidates.append(row[2])
         if row[2] not in people:
             people.append(row[2])
-        people_count = [candidates.count(ind) for ind in set(candidates)]
+        if people[0] == row[2]:
+            results[0] += 1
+        elif people[1] == row[2]:
+            results[1] += 1
+        elif people[2] == row[2]:
+            results[2] += 1
+        else:
+            results[3] += 1
         
 print(people)
-print(people_count)
+print(len(people))
+print(results)
             
 print("Election Results")
 print("-------------------")
 print(f"Total Votes: {len(candidates)}")
-
+print(f"{people[0]}: {(results[0]/len(candidates))*100}% ({results[0]})")
+print(f"{people[1]}: ({results[1]})")
+print(f"{people[2]}: ({results[2]})")
+print(f"{people[3]}: ({results[3]})")
 print("-------------------")
 # print(f"Winner: {}")
 print("-------------------")
