cef4386929979bd5d857f814dc1dd2c76d963976
shantipriya30 <75239249+shantipriya30@users.noreply.github.com>
2021-01-05 16:45:46 -0800

    Count of Total Votes

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,32 +1,30 @@
 import os
 import csv
 
+people =[]
 candidates = []
 percentage = []
 winner = []
 
 count = 0
 
-poll_csv = os.path.join('.','Resources','election_data.csv')
-    with open(poll_csv) as poll_file:
+poll_csv = os.path.join(".","Resources","election_data.csv")
+with open(poll_csv) as poll_file:
     csvreader = csv.reader(poll_file, delimiter = ',')
     csv_reader = next(csvreader)
     for row in csvreader:
-        if row[2] not in candidates: 
-            count += 1
-            candidates.append(row)
-    candidates.append(row[2])
-    
-
-
-
-
-
-
+        candidates.append(row[2])
+        if row[2] not in people:
+            people.append(row[2])
+        people_count = [candidates.count(ind) for ind in set(candidates)]
+        
+print(people)
+print(people_count)
+            
 print("Election Results")
 print("-------------------")
-print(f"Total Votes: {total votes}")
+print(f"Total Votes: {len(candidates)}")
 
 print("-------------------")
-print(f"Winner: {}")
+# print(f"Winner: {}")
 print("-------------------")
